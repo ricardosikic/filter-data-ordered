df56cbb3bcd82ebbe0c5069ce7d73e76820aeb84
ricardosikic <ricardo.sikic@gmail.com>
2020-01-07 15:15:16 -0300

    fixed function

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+let num = [1, 4, 5, 6];
+
 filter_data = (data) => {
     let order = data.sort();
-
     if(order.length <= 0) {
         console.log('Must have more than 1 element');
     }
     return order.filter((item, index) => {
-      return data.indexOf(item) === index;
+      return order.indexOf(item) === index;
     });
 }
 
